ccd06b9113b997bd03e14d12ea6a6e4b5dfb0478
TK <tgrl@protonmail.com>
2022-12-28 16:22:57 +0300

    Initial Commit

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+        private GameObject player;
+
+        private void Start()
+        {
+            player = GameObject.FindWithTag("Player");
+        }
+        private void Update()
+        {
+            if (DistanceToPlayer(player) < chaseDistance)
+            {
+                print(gameObject.name + " is chasing player");
+            }
+        }
+
+        private float DistanceToPlayer(GameObject player)
+        {
+
+            return Vector3.Distance(transform.position, player.transform.position);
+        }
+    }
+}
