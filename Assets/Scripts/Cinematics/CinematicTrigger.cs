5a93eca5a02eedf9618a49eb15fcb2f76ace4f43
TK <tgrl@protonmail.com>
2022-12-29 20:45:28 +0300

    Implemented cinematics behavior

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        bool isTriggered = false;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!isTriggered && other.CompareTag("Player"))
+            {
+                isTriggered = true;
+                GetComponent<PlayableDirector>().Play();
+            }
+        }
+    }
+}
