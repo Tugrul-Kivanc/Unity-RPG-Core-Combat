6b2f811941f63c4211b1771cd3d61e5fc5e13467
TK <tgrl@protonmail.com>
2022-12-30 16:01:30 +0300

    Implemented FadeOut and FadeIn

---
+++ Assets/Scripts/Scene Management/Fader.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {
+        CanvasGroup canvasGroup;
+
+        void Awake()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        private void Start()
+        {
+            StartCoroutine(FadeOutIn());
+        }
+
+        public IEnumerator FadeOut(float time)
+        {
+            while (canvasGroup.alpha < 1f)
+            {
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null;
+            }
+        }
+
+        public IEnumerator FadeIn(float time)
+        {
+            while (canvasGroup.alpha > 0f)
+            {
+                canvasGroup.alpha -= Time.deltaTime / time;
+                yield return null;
+            }
+        }
+
+        IEnumerator FadeOutIn()
+        {
+            yield return FadeOut(2f);
+            yield return FadeIn(1f);
+        }
+    }
+}
