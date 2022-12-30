34159498afb2527990838b7405cc189ce2aaea4e
TK <tgrl@protonmail.com>
2022-12-30 16:43:59 +0300

    Added saving system

---
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SavingSystem
+{
+    public class SavingSystem : MonoBehaviour
+    {
+        public void Save(string saveFile)
+        {
+            print("Saving to " + saveFile);
+        }
+
+        public void Load(string loadFile)
+        {
+            print("Loading from " + loadFile);
+        }
+    }
+}
---
+++ Assets/Scripts/Saving/SavingWrapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SavingSystem
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string defaultSaveFile = "save";
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F5))
+                GetComponent<SavingSystem>().Save(defaultSaveFile);
+            if (Input.GetKeyDown(KeyCode.F9))
+                GetComponent<SavingSystem>().Load(defaultSaveFile);
+        }
+    }
+}
