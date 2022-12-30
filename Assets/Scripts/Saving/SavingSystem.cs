d49565842ee9fc01136850e7140000f482971d1d
TK <tgrl@protonmail.com>
2022-12-30 19:14:22 +0300

    Changed manual serialization to built in c# one

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using UnityEngine;
 
@@ -9,60 +10,36 @@ namespace RPG.SavingSystem
 {
     public class SavingSystem : MonoBehaviour
     {
-        private string saveFileExtension = ".sav";
+        string saveFileExtension = ".sav";
+        BinaryFormatter formatter = new BinaryFormatter();
+        Transform player;
+        void Awake()
+        {
+            player = GameObject.FindWithTag("Player").transform;
+        }
         public void Save(string saveFile)
         {
             string path = GetPathFromSaveFile(saveFile);
-            print("Saving to " + path);
             using (FileStream stream = File.Open(path, FileMode.Create))
             {
-                Transform playerTransform = GameObject.FindWithTag("Player").transform;
-
-                byte[] buffer = SerializeVector(playerTransform.position);
-                stream.Write(buffer, 0, buffer.Length);
+                SerializableVector3 serializablePosition = new SerializableVector3(player.position);
+                formatter.Serialize(stream, serializablePosition);
             }
         }
 
         public void Load(string saveFile)
         {
             string path = GetPathFromSaveFile(saveFile);
-            print("Loading from " + path);
             using (FileStream stream = File.Open(path, FileMode.Open))
             {
-                byte[] buffer = new byte[stream.Length];
-                stream.Read(buffer, 0, buffer.Length);
-
-                Transform playerTransform = GameObject.FindWithTag("Player").transform;
-                playerTransform.position = DeserializeVector(buffer);
-
+                SerializableVector3 serializedPosition = (SerializableVector3)formatter.Deserialize(stream);
+                player.position = serializedPosition.ToVector();
             }
         }
 
         private string GetPathFromSaveFile(string saveFile)
         {
             return Path.Combine(Application.persistentDataPath, saveFile + saveFileExtension);
-        }
-
-        private byte[] SerializeVector(Vector3 vector)
-        {
-            byte[] vectorBytes = new byte[3 * sizeof(float)];
-
-            BitConverter.GetBytes(vector.x).CopyTo(vectorBytes, sizeof(float) * 0);
-            BitConverter.GetBytes(vector.y).CopyTo(vectorBytes, sizeof(float) * 1);
-            BitConverter.GetBytes(vector.z).CopyTo(vectorBytes, sizeof(float) * 2);
-
-            return vectorBytes;
-        }
-
-        private Vector3 DeserializeVector(byte[] buffer)
-        {
-            Vector3 vector = new Vector3();
-
-            vector.x = BitConverter.ToSingle(buffer, sizeof(float) * 0);
-            vector.y = BitConverter.ToSingle(buffer, sizeof(float) * 1);
-            vector.z = BitConverter.ToSingle(buffer, sizeof(float) * 2);
-
-            return vector;
         }
     }
 }
