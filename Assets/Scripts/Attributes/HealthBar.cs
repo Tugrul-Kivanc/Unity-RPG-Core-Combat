2ee9fd91ca5c73b6a8b77603a30cd38d6e3eecf2
TK <tgrl@protonmail.com>
2023-01-07 21:22:59 +0300

    Updated healthbar implementation, disabled healthbars of dead enemies

--- Assets/Scripts/Attributes/HealthBar.cs
+++ Assets/Scripts/Attributes/HealthBar.cs
@@ -8,8 +8,22 @@ namespace RPG.Attributes
     {
         [SerializeField] private Health healthComponent;
         [SerializeField] private RectTransform foreground;
-        private void Update()
+        [SerializeField] private Canvas rootCanvas;
+
+        private void Start()
         {
+            rootCanvas.enabled = false;
+        }
+
+        public void UpdateHealthBar(float value)
+        {
+            if (Mathf.Approximately(healthComponent.GetHealthFraction(), 0f))
+            {
+                rootCanvas.enabled = false;
+                return;
+            }
+
+            rootCanvas.enabled = true;
             foreground.localScale = new Vector3(healthComponent.GetHealthFraction(), 1f, 1f);
         }
     }
