8445449c09c7f66a0bb0280f15a0c5252ee33db0
TK <tgrl@protonmail.com>
2023-01-08 16:41:37 +0300

    Fixed Fade coroutine clash

--- Assets/Scripts/Scene Management/Fader.cs
+++ Assets/Scripts/Scene Management/Fader.cs
@@ -7,6 +7,9 @@ namespace RPG.SceneManagement
     public class Fader : MonoBehaviour
     {
         private CanvasGroup canvasGroup;
+        private Coroutine currentFadeRoutine;
+        private float fadeOutAlphaTarget = 1f;
+        private float fadeInAlphaTarget = 0f;
 
         private void Awake()
         {
@@ -15,18 +18,30 @@ namespace RPG.SceneManagement
 
         public IEnumerator FadeOut(float time)
         {
-            while (canvasGroup.alpha < 1f)
-            {
-                canvasGroup.alpha += Time.deltaTime / time;
-                yield return null;
-            }
+            return Fade(fadeOutAlphaTarget, time);
         }
 
         public IEnumerator FadeIn(float time)
         {
-            while (canvasGroup.alpha > 0f)
+            return Fade(fadeInAlphaTarget, time);
+        }
+
+        public IEnumerator Fade(float alphaTarget, float time)
+        {
+            if (currentFadeRoutine != null)
             {
-                canvasGroup.alpha -= Time.deltaTime / time;
+                StopCoroutine(currentFadeRoutine);
+            }
+
+            currentFadeRoutine = StartCoroutine(FadeRoutine(alphaTarget, time));
+            yield return currentFadeRoutine;
+        }
+
+        private IEnumerator FadeRoutine(float alphaTarget, float time)
+        {
+            while (!Mathf.Approximately(canvasGroup.alpha, alphaTarget))
+            {
+                canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, alphaTarget, Time.deltaTime / time);
                 yield return null;
             }
         }
